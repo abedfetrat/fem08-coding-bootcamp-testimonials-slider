3a48b7e93c432d348943226f74e935ee45967334
Abed Fetrat <abedfetrat@gmail.com>
2022-06-22 19:47:30 +0200

    update

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 const root = document.documentElement;
-const photo = document.querySelector('.photo');
+
+const testimonialSliders = document.querySelectorAll('.testimonials-slider');
+testimonialSliders.forEach((slider) => {
+    slider.querySelector('.previous').addEventListener('click', () => {
+        showSlide(-1);
+    });
+    slider.querySelector('.next').addEventListener('click', () => {
+        showSlide(1);
+    });
+
+    function showSlide(offset) {
+        const slides = slider.querySelector('.slides');
+        const activeIndex = [...slides.children].indexOf(slides.querySelector('.active'));
+        let newIndex = activeIndex + offset;
+
+        if (newIndex < 0) newIndex = slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
+
+        slides.children[newIndex].classList.add('active');
+        slides.children[activeIndex].classList.remove('active');
+    }
+});
 
 /* Updates photo height custom property with value of the photo elements height. */
 window.addEventListener('resize', () => {
+    const photo = document.querySelector('.photo');
     const height = photo.getBoundingClientRect().height;
     root.style.setProperty('--photo-height', height + 'px');
 })
